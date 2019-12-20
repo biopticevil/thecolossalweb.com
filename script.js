d716e08aa37abefd705f7fddcb3d1f8eef8ce736
biopticevil <57766692+biopticevil@users.noreply.github.com>
2019-12-20 12:32:42 -0500

    done

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+var question ="where was the first collosal computer made?";
+var answers = ['Rome', 'greece', 'Germany', 'Italy'];
+var answerButtons = document.getElementsByClassName("quizButton");
+var correct = 2;
+document.getElementById('question').innerHTML = question;
 
+for(var spot=0; spot<answers. length; spot++)
+{
+    answerButtons[spot].innerHTML = answers[spot];
+
+    answerButtons[spot].addEventListener('click', function() {
+        disableAll();
+        if(this.innerHTML == answers[correct]) {
+            this.setAttribute('style','background-color:rgb(76,175,80);');
+        } else {
+            this.setAttribute('style','background-color:rgb(210,50,35);');
+        }
+    });
+}
+
+
+function changeColor() {
+    document.getElementById('demo2').setAttribute('style','background-color:rgb(210,50,35);');
+}
+
+function disableAll() {
+    for(var num=0; num<answerButtons.length; num++) {
+        answerButtons[num].setAttribute('disabled',true)
+    }
+}
